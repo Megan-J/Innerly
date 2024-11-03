583a5a14005b899c36552b58aedcfdfb1382ce39
Jasmine Lao <jasmine.lao@sjsu.edu>
2024-11-02 21:18:17 -0700

    implemented pomodoro timer logic

--- app/src/main/java/edu/sjsu/android/jams/MainActivity.java
+++ app/src/main/java/edu/sjsu/android/jams/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +15,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            PomodoroFragment pomodoroFragment = new PomodoroFragment();
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            transaction.replace(R.id.fragment_pomodoro, pomodoroFragment);
+            transaction.commit();
+        }
     }
 }
--- app/src/main/java/edu/sjsu/android/jams/PomodoroFragment.java
+++ app/src/main/java/edu/sjsu/android/jams/PomodoroFragment.java
@@ -1,47 +1,37 @@
 package edu.sjsu.android.jams;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.os.CountDownTimer;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link PomodoroFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class PomodoroFragment extends Fragment {
+    private TextView timeText, sessionLengthText, breakLengthText, sessionStatus;
+    private Button startButton, resetButton, sessionIncrement, sessionDecrement, breakIncrement, breakDecrement;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private CountDownTimer countDownTimer;
+    private boolean timerRunning = false;
+    private boolean workSession = true;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private long workDuration = 1 * 60 * 1000;
+    private long breakDuration = 2 * 60 * 1000;
+    private long timeRemaining = workDuration;
 
     public PomodoroFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PomodoroFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static PomodoroFragment newInstance(String param1, String param2) {
         PomodoroFragment fragment = new PomodoroFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -49,16 +39,131 @@ public class PomodoroFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pomodoro, container, false);
+        View view = inflater.inflate(R.layout.fragment_pomodoro, container, false);
+        timeText = view.findViewById(R.id.timeText);
+        sessionLengthText = view.findViewById(R.id.sessionLengthText);
+        breakLengthText = view.findViewById(R.id.breakLengthText);
+        sessionStatus = view.findViewById(R.id.session_status);
+        startButton = view.findViewById(R.id.startButton);
+        resetButton = view.findViewById(R.id.resetButton);
+        sessionIncrement = view.findViewById(R.id.sessionIncrement);
+        sessionDecrement = view.findViewById(R.id.sessionDecrement);
+        breakIncrement = view.findViewById(R.id.breakIncrement);
+        breakDecrement = view.findViewById(R.id.breakDecrement);
+
+        sessionLengthText.setText(String.valueOf(workDuration / 60 / 1000));
+        breakLengthText.setText(String.valueOf(breakDuration / 60 / 1000));
+
+        startButton.setOnClickListener(v -> startOrPauseTimer());
+        resetButton.setOnClickListener(v -> resetTimer());
+        sessionIncrement.setOnClickListener(v -> adjustSessionTime(1));
+        sessionDecrement.setOnClickListener(v -> adjustSessionTime(-1));
+        breakIncrement.setOnClickListener(v -> adjustBreakTime(1));
+        breakDecrement.setOnClickListener(v -> adjustBreakTime(-1));
+
+        return view;
     }
+
+    private void startOrPauseTimer() {
+        if(timerRunning){
+            pauseTimer();
+        }
+        else{
+            startTimer();
+        }
+    }
+
+    private void startTimer() {
+        countDownTimer = new CountDownTimer(timeRemaining, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timeRemaining = (int) millisUntilFinished;
+//                startButton.setText("Pause");
+                updateTimerText();
+            }
+
+            @Override
+            public void onFinish() {
+                if(workSession){
+                    timeRemaining = breakDuration;
+                    workSession = false;
+                    sessionStatus.setText("Break Start!");
+                }
+                else{
+                    timeRemaining = workDuration;
+                    workSession = true;
+                    sessionStatus.setText("Session Start!");
+                }
+//                startTimer();
+                updateTimerText();
+            }
+        }.start();
+        timerRunning = true;
+//        startButton.setText("Pause");
+    }
+
+    @SuppressLint("DefaultLocale")
+    private void updateTimerText() {
+        int mins = (int) (timeRemaining / 1000) / 60;
+        int secs = (int) (timeRemaining / 1000) % 60;
+        timeText.setText(String.format("%02d:%02d", mins, secs));
+    }
+
+    private void pauseTimer() {
+        if(countDownTimer != null){
+            startButton.setText("Pause");
+            countDownTimer.cancel();
+        }
+        timerRunning = false;
+        startButton.setText("Start");
+    }
+
+    private void resetTimer() {
+        if(countDownTimer != null){
+            countDownTimer.cancel();
+        }
+        if(workSession){
+            timeRemaining = workDuration;
+            sessionStatus.setText("Session Start!");
+        }
+        else{
+            timeRemaining = breakDuration;
+            sessionStatus.setText("Break Start!");
+        }
+        updateTimerText();
+        timerRunning = false;
+        startButton.setText("Start");
+    }
+
+    private void adjustSessionTime(int i) {
+        long newSessionDuration = workDuration + (long) i * 60 * 1000;
+        if(newSessionDuration > 0){
+            workDuration = newSessionDuration;
+            sessionLengthText.setText(String.valueOf(workDuration / (60*1000)));
+            if(workSession){
+                timeRemaining = workDuration;
+                updateTimerText();
+            }
+        }
+    }
+
+    private void adjustBreakTime(int i) {
+        long newBreakDuration = breakDuration + (long) i * 60 * 1000;
+        if(newBreakDuration > 0){
+            breakDuration = newBreakDuration;
+            breakLengthText.setText(String.valueOf(breakDuration / (60*1000)));
+            if(!workSession){
+                timeRemaining = breakDuration;
+                updateTimerText();
+            }
+        }
+    }
+
+
+
 }
