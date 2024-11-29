5ce5f9f58662a19c817536cd012bc545bee57f22
Jasmine Lao <jasmine.lao@sjsu.edu>
2024-11-28 16:34:39 -0800

    added login and signup fragments

---
+++ app/src/main/java/edu/sjsu/android/jams/SignupFragment.java
@@ -0,0 +1,37 @@
+package edu.sjsu.android.jams;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SignupFragment extends Fragment {
+
+
+    public SignupFragment() {
+        // Required empty public constructor
+    }
+
+
+    public static SignupFragment newInstance(String param1, String param2) {
+        SignupFragment fragment = new SignupFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_signup, container, false);
+    }
+}
